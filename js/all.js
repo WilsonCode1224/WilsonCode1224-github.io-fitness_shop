7e71d8979da7a4ae82b650f404edecb19be33b32
WilsonCode1224 <smallmoney1224@gmail.com>
2023-08-17 10:42:38 +0800

    update files

--- js/all.js
+++ js/all.js
@@ -1,7 +1,7 @@
 // top-header 上所有的 h2,a 經過滑鼠移動以後變成黑色文字 , header 元素本身變成白色背景
 
+const header = document.querySelector("header");
 const topHeader = document.querySelector(".top-header");
-const childElements = topHeader.querySelector("a");
 
 topHeader.addEventListener("mouseenter", () => {
   topHeader.classList.add("topHeaderhover");
@@ -9,4 +9,17 @@ topHeader.addEventListener("mouseenter", () => {
 
 topHeader.addEventListener("mouseleave", () => {
   topHeader.classList.remove("topHeaderhover");
+});
+
+// css .topHeaderforscroll
+
+window.addEventListener("scroll", () => {
+  const scrollPosition = window.scrollY; // 計算當下
+
+  // 如果滾動位置大於0，則套用樣式；否則取消樣式
+  if (scrollPosition > 0) {
+    topHeader.classList.add("topHeaderforscroll");
+  } else {
+    topHeader.classList.remove("topHeaderforscroll");
+  }
 });
